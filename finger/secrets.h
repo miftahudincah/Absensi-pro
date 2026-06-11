7f117bdd797b9f1ed433c9ffbdd9876214713647
miftahudincah <49975614+miftahudincah@users.noreply.github.com>
2026-06-11 15:57:11 +0700

    Add files via upload

---
+++ finger/secrets.h
@@ -0,0 +1,9 @@
+// secrets.h - Konfigurasi Firebase
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// Konfigurasi Firebase
+#define API_KEY "AIzaSyBZg9NpbBAg8dKHkCbYf4J_2bpHH2ZJWWI"
+#define DATABASE_URL "https://absensi-4389a-default-rtdb.firebaseio.com/"
+
+#endif
